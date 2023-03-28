35f450070e4942072c5d00c856e112e9aad261c1
Brian Carper <brian@carper.ca>
2023-03-28 12:45:25 -0700

    add hints toggle; fix layout

--- spelling-bee-hints.user.js
+++ spelling-bee-hints.user.js
@@ -15,23 +15,52 @@
 (function() {
     'use strict';
     window.addEventListener("load", (e) => {
-        const controls = document.getElementsByClassName('sb-controls-box')[0];
-        const hints = document.createElement('div');
-        const hintLetters = document.createElement('div');
-        const progressBox = document.getElementsByClassName('sb-progress-box')[0];
-        hints.style = 'column-count: 3; font-family: monospace; padding-left: 2rem; padding-right: 2rem;';
-        hintLetters.style = 'font-family: monospace; background: #f7da21; padding: 1rem; font-weight: bold; border-radius: 1rem;';
+        const hintsContainer = document.createElement('div');
+        const hintsToggle = document.createElement('div');
+        let hintsVisible;
 
-        document.getElementById('pz-game-root').prepend(progressBox);
+        let toggleHints = function() {
+            hintsVisible = !hintsVisible;
+            hintsContainer.style.setProperty('display', hintsVisible ? 'flex' : 'none');
+            hintsToggle.style.setProperty('background', hintsVisible ? '#e6e6e6' : 'transparent');
+        }
+
+        const monospace = "ui-monospace, 'Cascadia Code', 'Source Code Pro', Menlo, Consolas, 'DejaVu Sans Mono', monospace";
+
+        const hints = document.createElement('div');
+        hints.style = 'column-count: 3; padding-right: 1.5rem;';
+        hints.style.setProperty('font-family', monospace);
+        hints.style.setProperty('font-size', '0.8rem');
+
+        const hintLetters = document.createElement('div');
+        hintLetters.style = "background: #f7da21; padding: 1rem; font-weight: bold; border-radius: 1rem;";
+        hintLetters.style.setProperty('font-family', monospace);
+        hintLetters.style.setProperty('font-size', '0.8rem');
+        const progressBox = document.getElementsByClassName('sb-progress-box')[0];
         progressBox.style.setProperty('display', 'relative');
+        document.getElementById('pz-game-root').prepend(progressBox);
+
         document.getElementsByClassName('sb-status-box')[0].style = 'display: none';
 
         const pangrams = new Set(window.gameData.today.pangrams.map(a => a.toUpperCase()));
         const uppercaseWords = window.gameData.today.answers.map(a => a.toUpperCase())
 
-        controls.appendChild(hints);
-        controls.appendChild(hintLetters);
+        hintsVisible = false;
+        hintsContainer.style.setProperty('display', 'none');
+        hintsContainer.style.setProperty('margin-left', '3.5rem');
+        hintsContainer.appendChild(hints);
+        hintsContainer.appendChild(hintLetters);
+
+        const controls = document.getElementsByClassName('sb-controls-box')[0];
         controls.style.setProperty('padding-top', '60px');
+        controls.appendChild(hintsContainer);
+
+        hintsToggle.className = 'hive-action hive-action__submit sb-touch-button';
+        hintsToggle.innerHTML = 'Hints';
+        hintsToggle.addEventListener('click', () => toggleHints(hintsContainer));
+
+        const hiveActions = document.getElementsByClassName('hive-actions')[0];
+        hiveActions.appendChild(hintsToggle);
 
         let updateHints = function(hints) {
             hints.innerHTML = null;
@@ -39,6 +68,7 @@
             let starts = {};
 
             const alreadyKnown = new Set([...document.getElementsByClassName('sb-anagram')].map(e => e.innerHTML.toUpperCase()));
+            const alreadyKnownPangrams = new Set([...alreadyKnown].filter(a => pangrams.has(a)));
             const sortedWords = [...uppercaseWords.filter(a => alreadyKnown.has(a)),
                                  ...uppercaseWords.filter(a => !alreadyKnown.has(a)).sort((a,b) => a[0].localeCompare(b[0]) || (a.length - b.length))]
             sortedWords.forEach(a => {
@@ -60,10 +90,13 @@
                     starts[a.slice(0,2)] += 1;
                 }
             });
-            hintLetters.innerHTML += '<div style="padding-bottom: 1rem">Words found: ' + alreadyKnown.size + ' of ' + window.gameData.today.answers.length + '</div>';
-            hintLetters.innerHTML += '<div style="padding-bottom: 1rem"> ' + window.gameData.today.pangrams.length + ' pangrams<br></div>';
+            hintLetters.innerHTML += '<div>Found:</div>';
+            hintLetters.innerHTML += `<div>&nbsp;${alreadyKnown.size} of ${window.gameData.today.answers.length}</div>`;
+            hintLetters.innerHTML += `<div style="padding-bottom: 1rem">&nbsp;${alreadyKnownPangrams.size} of ${window.gameData.today.pangrams.length} pangrams</div>`;
 
-            hintLetters.innerHTML += Object.keys(starts).sort().map(k => '<div style="white-space: nowrap">' + k + ': ' + starts[k] + '</div>').join('');
+            hintLetters.innerHTML += '<div>Remaining:</div>';
+
+            hintLetters.innerHTML += Object.keys(starts).sort().map(k => '<div style="white-space: nowrap">&nbsp;' + k + ': ' + starts[k] + '</div>').join('');
             if(alreadyKnown.size === window.gameData.today.answers.length) {
                 hintLetters.innerHTML += '<span style="font-size: 2.5rem;">👑🐝 ❤️</span>';
             }
